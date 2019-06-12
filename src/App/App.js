e379ddbb3860d729c5d3ee8fb83af4d12f6548b7
Trystan Newbury <trystan.newbury@thoughtworks.com>
2019-06-12 17:44:00 +0100

    [Amalie|Trystan] Added monitoring for checkbox status

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
+import { Button, Divider, Grid, Header, Icon, Search, Segment } from 'semantic-ui-react'
+
 import './App.css';
 import ToDoHeader from '../Components/ToDoHeader';
 import ToDoList from '../Components/ToDoList';
+import CreateForm from '../Components/CreateToDo';
 
 class App extends React.Component {
 
   state = {
-    todos: []
+    userId: 1, 
+    todos: [],
+    newTodo: {}
   }
 
   async componentDidMount() {
@@ -18,16 +23,50 @@ class App extends React.Component {
   }
 
   filterTodos = (todos, userId) => todos.filter(todo => 
-      todo.userId === userId
+    todo.userId === userId
   );
+
+  handleTodoChange = (event, data) => {
+    let value;
+    let name;
+    if (!data) {
+      value = event.target.value;
+      name = event.target.name;
+    } else {
+      value = data.checked;
+      name = data.name;
+    }
+    this.setState({
+      newTodo: {
+        ...this.state.newTodo, [name]: value
+      }
+    })
+  }
+
+  createTodo = (event) => {
+    console.log('created!')
+    // grab this.state.newTodo
+    // add this to our Todos array in state (200 todos)
+    // spread operater, new tosdo
+  }
 
   render() {
     // const { todos } = this.state
-    const todos = this.filterTodos(this.state.todos, 2);
+    const { handleTodoChange, createTodo } = this
+    const todos = this.filterTodos(this.state.todos, this.state.userId);
     return (
       <div className="App">
         <ToDoHeader/>
-        <ToDoList todos={todos}/>
+        <Segment placeholder>
+          <Grid columns={2} stackable textAlign='left'>
+            <Grid.Column>
+              <ToDoList todos={todos}/>
+            </Grid.Column>
+            <Grid.Column>
+              <CreateForm handleTodoChange={handleTodoChange} createTodo={createTodo}/>
+            </Grid.Column>
+          </Grid>
+        </Segment>
       </div>
     );
   }
